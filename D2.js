0e56ec691a45315607c23523dfc82fd7dd2ba315
Vidied <davidepan2001@gmail.com>
2026-03-10 16:32:26 +0100

    1

--- D2.js
+++ D2.js
@@ -3,24 +3,68 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let n1 = 1;
+let n2 = 2;
+
+if (n1 > n2) {
+  console.log(n1, "è minore di", n2);
+} else {
+  console.log(n2, "è maggiore di", n1);
+}
 
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let n5 = 5;
+
+if (n1 === n5) {
+  console.log(n1, "è uguale a", n5);
+} else {
+  console.log(n1, "non è uguale a", n5);
+}
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let n45 = 45;
+
+let divisibile = n45 % n5;
+console.log(divisibile);
+
+if (divisibile === 0) {
+  console.log(n45, "è divisibile per 5");
+} else {
+  console.log(n45, "non è divisibile per 5");
+}
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let n4 = 4;
+let n8 = 8;
+
+// test
+let prova8 = n4 + n4 === 8;
+console.log(prova8); //true
+
+prova8 = n4 + n2 === 8;
+console.log(prova8); //false
+
+if (n4 === 8 || n4 + n4 === 8 || n8 === 8) {
+  console.log(
+    "con",
+    n4,
+    "e",
+    n8,
+    "si ottiene direttamente un valore di 8 o lo prendono sommandosi/sottraendosi",
+  );
+}
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -30,12 +74,86 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let totalShoppingCart = 15;
+let minSped = 50;
+let costSped = 10;
+let totale = totalShoppingCart + costSped;
+if (totalShoppingCart >= 50) {
+  totale = totalShoppingCart;
+}
+
+if (totalShoppingCart >= minSped) {
+  console.log(
+    "hai un carrello di",
+    totalShoppingCart,
+    "hai diritto alla promozione della spedizione gratuita, hai risparmiato",
+    costSped,
+  );
+} else {
+  console.log(
+    "non hai raggiunto la quota minima per la promo spedizione gratuita, il tuo totale sarà di",
+    totalShoppingCart,
+    "+",
+    costSped,
+    "=",
+    totale,
+  );
+}
+
+// prova
+totalShoppingCart = 55;
+if (totalShoppingCart >= minSped) {
+  console.log(
+    "hai un carrello di",
+    totalShoppingCart,
+    "hai diritto alla promozione della spedizione gratuita, hai risparmiato",
+    costSped,
+  );
+} else {
+  console.log(
+    "non hai raggiunto la quota minima per la promo spedizione gratuita, il tuo totale sarà di",
+    totalShoppingCart,
+    "+",
+    costSped,
+    "=",
+    totale,
+  );
+}
+
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let blackFriday = true;
+if (blackFriday === true) {
+  totalShoppingCart = totalShoppingCart * 0.8;
+}
+if (totalShoppingCart >= 50) {
+  totale = totalShoppingCart;
+} else {
+  totale = totalShoppingCart + costSped;
+}
+
+if (totalShoppingCart >= minSped) {
+  console.log(
+    "hai un carrello di",
+    totalShoppingCart,
+    "hai diritto alla promozione della spedizione gratuita, hai risparmiato",
+    costSped,
+  );
+} else {
+  console.log(
+    "non hai raggiunto la quota minima per la promo spedizione gratuita, il tuo totale sarà di",
+    totalShoppingCart,
+    "+",
+    costSped,
+    "=",
+    totale,
+  );
+}
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -44,18 +162,52 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let n11 = 11;
+let n12 = 12;
+let n13 = 13;
+
+if (n11 > n12 && n11 > n13) {
+  if (n12 > n13) {
+    console.log(n11, n12, n13);
+  } else console.log(n11, n13, n12);
+} else if (n12 > n11 && n12 > n13) {
+  if (n11 > n13) {
+    console.log(n12, n11, n13);
+  } else console.log(n12, n13, n11);
+} else if (n11 > n12) {
+  console.log(n13, n11, n12);
+} else console.log(n13, n12, n11);
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let nome = "Cinzia";
+
+if (typeof nome === "string") {
+  console.log(nome, "è una string");
+} else {
+  console.log(nome, "è un numero");
+}
+
+if (typeof n1 === "string") {
+  console.log(n1, "è una string");
+} else {
+  console.log(n1, "è un numero");
+}
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+if (n11 % 2 === 0) {
+  console.log(n11, "è un numero pari");
+} else {
+  console.log(n11, "è un numero disppari");
+}
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -71,15 +223,42 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let val = 7;
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
+
+val = 3;
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
+
+val = 11;
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
+
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
 
 const me = {
-  name: 'John',
-  lastName: 'Doe',
-  skills: ['javascript', 'html', 'css'],
-}
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
+};
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
